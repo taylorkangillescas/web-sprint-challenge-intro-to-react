2515b3f4174c8bc43c09acb52281f2d3b04d27f3
Taylor Kang-Illescas <taylorkangillescas@Taylors-MacBook-Pro-8.local>
2021-02-19 16:46:00 -0800

    all done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  const [ characters, setCharacters ] = useState([]);
+  useEffect(()=>{
+      axios.get('http://swapi.dev/api/people/')
+      .then((res)=>{
+          console.log(res.data.results)
+          setCharacters(res.data.results)
+      })
+      .catch((err)=>{
+          console.log(err)
+      })
+  }, [])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character character={characters}/>
+      
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,39 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+const CharacterContainer=styled.div`
+    margin: auto;
+    padding: 10px;
+    width: 25%;
+`
+const CharacterDiv = styled.div`
+    opacity: 1;
+    color: black;
+    margin: 1rem auto;
+    border: 1px solid black;
+    border-radius: 3px;
+    padding: 10px;
+    background-color: white;
+    text-align: center;
+`
+function Character(props){
+    const { character } = props
+    return (
+        <CharacterContainer>
+            {character.map((character)=>{
+             return <CharacterDiv>
+                <h2>{character.name}</h2>
+                <p>Height: {character.height}</p>
+                <p>Mass: {character.mass}</p>
+                <p>Hair Color: {character.hair_color}</p>
+                <p>Skin Color: {character.skin_color}</p>
+                <p>Eye Color: {character.eye_color}</p>
+                <p>Birth Year: {character.birth_year}</p>
+                <p>Gender: {character.gender}</p>
+                 </CharacterDiv>;
+             })}
+        </CharacterContainer>
+    )
+}
+export default Character
